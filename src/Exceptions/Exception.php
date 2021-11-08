407c863460df9e0a825963c6401063c597fd4284
Chin Leung <hello@chinleung.com>
2021-11-08 08:31:05 -0500

    Fix non numeric exception codes

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -6,4 +6,19 @@ use Exception as BaseException;
 
 class Exception extends BaseException
 {
+    /**
+     * Create a new instance of the exception.
+     *
+     * @param  string  $message
+     * @param  string  $code
+     */
+    public function __construct(string $message, string $code)
+    {
+        if (! is_numeric($code)) {
+            $message = "[{$code}]: {$message}";
+            $code = preg_replace('/[^\d]+/', '', $code);
+        }
+
+        parent::__construct($message, $code);
+    }
 }
